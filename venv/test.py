71d71e1e2a3be03e9e64b27abb44761715dc4f41
FelosoF <felosof89@gmail.com>
2024-09-11 17:36:15 +0300

    Atomic copy

--- venv/test.py
+++ venv/test.py
@@ -1,19 +1,36 @@
-class ReversedSequence:
-    def __init__(self, item):
-        self.item = item
+import copy
+class Atomic:
+    def __init__(self, data, deep = False):
+        self.data = data
+        self.deep = deep
+        self.tmp_data = copy.deepcopy(self.data) if self.deep else copy.copy(self.data)
 
-    def __getitem__(self, key):
-        return list(reversed(self.item))[key]
 
-    def __len__(self):
-        return len(self.item)
+    def __enter__(self):
+            return self.tmp_data
 
-numbers = [1, 2, 3, 4, 5]
-reversed_numbers = ReversedSequence(numbers)
-print(len(reversed_numbers))
 
-numbers.append(6)
-numbers.append(7)
-print(len(reversed_numbers))
-print()
-print('main')
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_val:
+            return True
+        else:
+            if isinstance(self.data, list):
+                self.data[:] = self.tmp_data
+            elif isinstance(self.data, dict|set):
+                self.data.clear()
+                self.data.update(self.tmp_data)
+        return False
+
+
+numbers = {1, 2, 3, 4, 5}
+
+with Atomic(numbers) as atomic:
+    atomic.add(6)
+    atomic.append(7)           # добавление элемента с помощью несуществующего метода
+
+print(sorted(numbers))
+
+with Atomic(numbers) as atomic:
+    atomic.add(6)
+
+print(sorted(numbers))
